198f96b4aceedb6596f68eed5c98b99a18c19df7
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-29 15:46:36 +0400

    Initialize class patient and attributes

---
+++ Hospital Management System/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System
+{
+    public class Patient : person
+    {
+         public int PatientID;
+         public string Ailment;
+         public string AssignedDoctor;
+         public string Room;
+        public Patient(string Name,int age,Gender gender): base (Name,age,gender) {
+            
+
+        }    
+
+    }
+}
