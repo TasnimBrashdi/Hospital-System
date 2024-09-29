36731ca2f5ef1cfe193e841062cda25422e5c548
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-29 15:30:01 +0400

    Create class with Attributes,Constructor and Method

---
+++ Hospital Management System/person.cs
@@ -0,0 +1,37 @@
+﻿using Hospital_Management_System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System
+{
+    public class person
+    {
+    
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public  Gender gen { get; set; }
+        public enum Gender
+        {
+            Male,
+            Female,
+            Other
+        }
+        public person(string Name, int Age,Gender gender)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            Gender gen =gender;
+          
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($" Name: {Name}\n Age:{Age} \nGender: {gen}");
+        }
+    }
+   
+}
+
+
