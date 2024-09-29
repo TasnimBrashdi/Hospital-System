df793510dd58f2abfb348db516fa0dc59d751aa1
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-29 16:38:15 +0400

    add attributes

--- Hospital Management System/Doctor.cs
+++ Hospital Management System/Doctor.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Hospital_Management_System
 {
-    public class Doctor
+    public class Doctor : person
     {
+        public int DoctorID;
+        public string Specialization;
+     ///continus
+        public Doctor(string Name, int age, Gender gender, int PatientID, string Ailment, Doctor AssignedDoctor, Room Room) : base(Name, age, gender)
+        {
+          
+
+        }
+    
     }
 }
