b7a7790f60a615f61603200aa21c2b8ad0141a49
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-29 16:37:27 +0400

    update

--- Hospital Management System/Patient.cs
+++ Hospital Management System/Patient.cs
@@ -11,19 +11,24 @@ namespace Hospital_Management_System
          public int PatientID;
          public string Ailment;
          public Doctor AssignedDoctor;
-         public string Room;
-        public Patient(string Name,int age,Gender gender,int PatientID, string Ailment,Doctor AssignedDoctor,string Room) : base (Name,age,gender) {
+         public Room Room;
+        public Patient(string Name,int age,Gender gender,int PatientID, string Ailment,Doctor AssignedDoctor,Room Room) : base (Name,age,gender) {
             this.PatientID = PatientID;
             this.Ailment = Ailment;
             this.AssignedDoctor = AssignedDoctor;
             this.Room = Room;
 
         }
-        public void AssignRoom(string Room)
+        public void AssignRoom(Room Room)
         {
+            
 
         }
         public void Discharge()
+        {
+            Room = null;
+        }
+        public  void DisplayInfo(int PatientID, string Ailment)
         {
 
         }
