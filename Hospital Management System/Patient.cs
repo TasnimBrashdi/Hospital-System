de55361ddcf8680141fb3313edac823834a8126c
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-29 16:16:31 +0400

    Add Methods

--- Hospital Management System/Patient.cs
+++ Hospital Management System/Patient.cs
@@ -10,12 +10,23 @@ namespace Hospital_Management_System
     {
          public int PatientID;
          public string Ailment;
-         public string AssignedDoctor;
+         public Doctor AssignedDoctor;
          public string Room;
-        public Patient(string Name,int age,Gender gender): base (Name,age,gender) {
-            
+        public Patient(string Name,int age,Gender gender,int PatientID, string Ailment,Doctor AssignedDoctor,string Room) : base (Name,age,gender) {
+            this.PatientID = PatientID;
+            this.Ailment = Ailment;
+            this.AssignedDoctor = AssignedDoctor;
+            this.Room = Room;
 
-        }    
+        }
+        public void AssignRoom(string Room)
+        {
+
+        }
+        public void Discharge()
+        {
+
+        }
 
     }
 }
