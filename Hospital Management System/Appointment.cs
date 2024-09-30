fe38ac7f06295c3521d1f233fae2eb5c81a68c5f
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-30 09:31:15 +0400

    add Attributes and Constructor: Initialize attributes to class Appointment

--- Hospital Management System/Appointment.cs
+++ Hospital Management System/Appointment.cs
@@ -8,6 +8,15 @@ namespace Hospital_Management_System
 {
     public class Appointment
     {
+        public Patient Patient { get; set; }
+        public Doctor Doctor { get; set; }
+        public DateTime AppointmentDate;
+        public Appointment(Patient Patient, Doctor Doctor, DateTime AppointmentDate)
+        {
+            this.Patient = Patient;
+            this.Doctor = Doctor;
+            this.AppointmentDate = AppointmentDate;
 
+        }
     }
 }
