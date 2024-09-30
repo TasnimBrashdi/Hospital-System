bc22c093afdb3cd5fd75831527b58659708fcfec
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-30 09:26:34 +0400

    ADD Attributes and Constructor class room

--- Hospital Management System/Room.cs
+++ Hospital Management System/Room.cs
@@ -8,5 +8,20 @@ namespace Hospital_Management_System
 {
     public class Room
     {
+        public int RoomNumber { get; set; }
+        public RoomType R { get; set; }
+        public enum RoomType
+        {
+            General, ICU, OperationTheater
+        }
+        public bool IsOccupied { get; set; }
+        
+        public Room(int RoomNumber, RoomType R,bool IsOccupied)
+        {
+            this.RoomNumber = RoomNumber;
+            this.R = R; 
+            this.IsOccupied = IsOccupied;
+            }
+
     }
 }
