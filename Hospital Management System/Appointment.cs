3b728e69697f20e956cd0958aed8beeeab6f9a36
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-30 10:17:40 +0400

    ADD METHODS TO CL;ASS Appointment

--- Hospital Management System/Appointment.cs
+++ Hospital Management System/Appointment.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Hospital_Management_System
 {
@@ -17,6 +18,24 @@ namespace Hospital_Management_System
             this.Doctor = Doctor;
             this.AppointmentDate = AppointmentDate;
 
+        }
+        public void ScheduleAppointment(DateTime date)
+        {
+            AppointmentDate = date;
+            Console.WriteLine($"Patient's Name:{Patient.Name} Doctor' Name: {Doctor.Name}  Appointment Date: {AppointmentDate}");
+        }
+        public void CancelAppointment(DateTime date) {
+
+
+            AppointmentDate = date;
+            Console.WriteLine($"Patient's Name:{Patient.Name} Doctor' Name: {Doctor.Name}  Appointment Date: {AppointmentDate}");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Canceled");
+
+        }
+        public void GetAppointmentDetails()
+        {
+            Console.WriteLine($"Patient's Name:{Patient.Name} Doctor' Name: {Doctor.Name}  Appointment Date: {AppointmentDate}");
         }
     }
 }
