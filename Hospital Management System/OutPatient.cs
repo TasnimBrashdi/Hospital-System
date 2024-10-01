649c4cc7eaa4b2c6d022076d73ed93be128af50d
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-01 11:17:22 +0400

    add class OutPatient and Initialize Constructor and Attributes

---
+++ Hospital Management System/OutPatient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Hospital_Management_System.person;
+
+namespace Hospital_Management_System
+{
+    public class OutPatient :Patient
+    {
+        public Clinic ClinicAssigned { get; set; }
+        public OutPatient(int PatientID, string Name, int age, Gender gender, string Ailment, Clinic ClinicAssigned) : base(PatientID, Name, age, gender, Ailment)
+        {
+            this.ClinicAssigned = ClinicAssigned;
+
+        }
+    }
+    
+}
