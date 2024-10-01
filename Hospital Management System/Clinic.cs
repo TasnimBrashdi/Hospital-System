e311a642d7d7308c3e4c9c1cbd990a8ce8c6c99a
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-01 10:39:12 +0400

    Initialize Constructor in class clinic

--- Hospital Management System/Clinic.cs
+++ Hospital Management System/Clinic.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Hospital_Management_System
 {
-    internal class Clinic
+    public class Clinic
     {
+        public int ClinicID { get; set; }
+        public string ClinicName { get; set; }
+        public Specialization Special { get; set; }
+        public enum Specialization
+        {
+            Cardiology, Neurology, Dermatology
+        }
+        public List<Room> Rooms;
+        public Dictionary<Doctor, List<Appointment>> AvailableAppointments;
+
+
+        public Clinic(int ClinicID, string ClinicName, Specialization Special, List<Room> Rooms, Dictionary<Doctor, List<Appointment>> AvailableAppointments) {
+        this.ClinicID = ClinicID;
+        this.ClinicName = ClinicName;
+            this.Special = Special;
+            this.Rooms = Rooms;
+            this.AvailableAppointments = AvailableAppointments;
+        }
     }
 }
