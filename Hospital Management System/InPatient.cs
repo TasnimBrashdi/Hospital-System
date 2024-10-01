17df3edc05a29035cd22b25058ee60cebedf9dc8
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-01 11:10:53 +0400

    add class inpatient and Initialize Constructor and Attributes

---
+++ Hospital Management System/InPatient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System
+{
+    public  class InPatient : Patient
+    {
+        public Room Room { get; set; }
+        public Doctor AssignedDoctor { get; set; }
+        public DateTime AdmissionDate { get; set; } 
+    public InPatient (int PatientID, string Name, int age, Gender gender, string Ailment, Doctor AssignedDoctor, Room Room, DateTime AdmissionDate) : base (PatientID, Name, age, gender,Ailment, AssignedDoctor)
+        {
+            this.Room = Room;
+            this.AssignedDoctor = AssignedDoctor;
+            this.AdmissionDate = AdmissionDate;
+
+        }
+    }
+}
