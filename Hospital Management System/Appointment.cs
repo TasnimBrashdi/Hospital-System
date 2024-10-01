6be3e32d0eec01ad92c5537ea98f734fe831d498
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-01 14:28:08 +0400

    update Appointment class

--- Hospital Management System/Appointment.cs
+++ Hospital Management System/Appointment.cs
@@ -12,31 +12,51 @@ namespace Hospital_Management_System
         public Patient Patient { get; set; }
         public Doctor Doctor { get; set; }
         public DateTime? AppointmentDate;
+        public TimeSpan AppointmentTime;
+        public bool IsBooked { get; set; }
         public Appointment(Patient Patient, Doctor Doctor, DateTime AppointmentDate)
         {
             this.Patient = Patient;
             this.Doctor = Doctor;
             this.AppointmentDate = AppointmentDate;
-
+    
         }
         public void ScheduleAppointment(DateTime date)
         {
             AppointmentDate = date;
+            IsBooked = true;
 
         }
         public void CancelAppointment(DateTime date) {
 
-            this.Patient = null;
-            this.Doctor = null;
+            if (Patient != null && Doctor != null)
+            {
+                Console.WriteLine($"Patient's Name: {Patient.Name}, Doctor's Name: {Doctor.Name}, Appointment Date: {AppointmentDate}");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Canceled");
+            }
+            else
+            {
+                Console.WriteLine("No appointment to cancel.");
+            }
+
+        
+            Patient = null;
+            Doctor = null;
             AppointmentDate = null;
-            Console.WriteLine($"Patient's Name:{Patient.Name} Doctor' Name: {Doctor.Name}  Appointment Date: {AppointmentDate}");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Canceled");
+            IsBooked = false;
 
         }
         public void GetAppointmentDetails()
         {
-            Console.WriteLine($"Patient's Name:{Patient.Name} Doctor' Name: {Doctor.Name}  Appointment Date: {AppointmentDate}");
+            if (IsBooked)
+            {
+                Console.WriteLine($"Patient's Name: {Patient?.Name}, Doctor's Name: {Doctor?.Name}, Appointment Date: {AppointmentDate}");
+            }
+            else
+            {
+                Console.WriteLine("No appointment details available.");
+            }
         }
     }
 }
