b8d0512c4c304762491de2171e8f95f977c5bc18
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-01 14:27:45 +0400

    update patient class

--- Hospital Management System/Patient.cs
+++ Hospital Management System/Patient.cs
@@ -10,28 +10,17 @@ namespace Hospital_Management_System
     {
          public int PatientID;
          public string Ailment;
-         public Doctor AssignedDoctor;
-         public Room Room;
-        public Patient(int PatientID, string Name,int age,Gender gender, string Ailment,Doctor AssignedDoctor) : base (Name,age,gender) {
+         //public Doctor AssignedDoctor;
+         //public Room Room;
+        public Patient(int PatientID, string Name,int age,Gender gender, string Ailment) : base (Name,age,gender) {
             this.PatientID = PatientID;
             this.Ailment = Ailment;
-            this.AssignedDoctor = AssignedDoctor;
+            //this.AssignedDoctor = AssignedDoctor;
           
 
         }
-        public void AssignRoom(Room Room)
-        {
-            this.Room = Room;
-            Room.OccupyRoom();
-            Console.WriteLine($" {Name} assigned to room {Room.RoomNumber} ");
-
-        }
-        public void Discharge()
-        {
-            Room.VacateRoom();
-            Room = null;
-         
-        }
+       
+      
         public override void DisplayInfo()
         {
             base.DisplayInfo();
