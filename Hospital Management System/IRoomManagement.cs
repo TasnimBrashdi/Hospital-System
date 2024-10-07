daf3c49f36416708484e183701d26af79c969890
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-07 16:37:46 +0400

    UPDATE  IRoomManagement interface

---
+++ Hospital Management System/IRoomManagement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System
+{
+    public interface IRoomManagement
+    {
+        void OccupyRoom();
+        void VacateRoom();
+        void DisplayRoomInfo();
+    }
+}
