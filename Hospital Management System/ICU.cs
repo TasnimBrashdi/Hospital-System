9f9f6d6920eab2bc123837321ab2b55341b34cdd
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-08 09:23:09 +0400

    create ICU class

---
+++ Hospital Management System/ICU.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System
+{
+    public class ICU : IRoomManagement
+    {
+        public int ICUNumber { get; set; }
+        public bool IsOccupied { get; set; }
+        public ICU(int ICUNumber)
+        {
+            this.ICUNumber = ICUNumber;
+            IsOccupied = false;
+        }
+        public void OccupyRoom()
+        {
+            IsOccupied = true;
+        }
+        public void VacateRoom()
+        {
+            IsOccupied = false;
+        }
+        public void DisplayRoomInfo()
+        {
+            Console.WriteLine(" - - - ICU Information - - - ");
+            Console.WriteLine($"ICU Number: {ICUNumber} Is Occupied  {IsOccupied}");
+        }
+        public bool IsRoomOccupied()
+        {
+            return IsOccupied;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
