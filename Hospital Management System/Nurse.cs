e8d4bbb842308e1125d176668c44333c924774f6
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-08 11:32:50 +0400

    Create class Nurse and Attributes,Methods

---
+++ Hospital Management System/Nurse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System
+{
+    public class Nurse : person
+    {
+        public int NurseID;
+        public Clinic AssignedClinic;
+        public Specializations SpecialNurse { get; set; }
+        public enum Specializations
+        {
+            General, Pediatrics, Surgery
+        }
+        public List<Patient> AssignedPatients;
+
+        public Nurse(int NurseID, string Name, int age, Gender gender, Specializations SpecialNurse, Clinic AssignedClinic) : base(Name, age, gender)
+        {
+            AssignedPatients = new List<Patient>();
+            this.NurseID = NurseID;
+            this.AssignedClinic = AssignedClinic;
+            this.SpecialNurse = SpecialNurse;
+
+        }
+    }
+}
