a2dbc5c1c00e4183dbbaada3d9e3c4302acea231
Tasnim <tasnimalbrashdi@gmail.com>
2024-10-08 09:22:14 +0400

    create Ward class

---
+++ Hospital Management System/Word.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System
+{
+    public class Ward: IRoomManagement
+    {
+
+        public int WardNumber { get; set; }
+        public int BedCount { get; set; }
+        public int OccupiedBeds { get; set; }
+        public Ward(int wardNumber, int bedCount)
+        {
+            WardNumber = wardNumber;
+            BedCount = bedCount;
+            OccupiedBeds = 0;
+        }
+        public void OccupyRoom()
+        {
+            if (OccupiedBeds < BedCount)
+            {
+                OccupiedBeds++;
+            }
+            else
+            {
+                Console.WriteLine("Ward is fully occupied.");
+            }
+        }
+        public void VacateRoom()
+        {
+            if (OccupiedBeds > 0)
+            {
+                OccupiedBeds--;
+            }
+            else
+            {
+                Console.WriteLine("Ward is already empty.");
+            }
+        }
+        public bool IsRoomOccupied()
+        {
+            return OccupiedBeds > 0;
+        }
+        public void DisplayRoomInfo()
+        {
+            Console.WriteLine(" - - - Ward Information - - - ");
+            Console.WriteLine($"Ward Number: {WardNumber} \nBed Count: {BedCount} \nOccupied Beds: {OccupiedBeds}");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
